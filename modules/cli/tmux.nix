ae969b1fb9c22b10d6d3c249184e4416e101fe91
curtself <curtself.cs@gmail.com>
2026-08-10 01:26:56 -0700

    Added fix for prefix indicator. Fixed resurrect key bindings.

--- modules/cli/tmux.nix
+++ modules/cli/tmux.nix
@@ -42,9 +42,33 @@
     extraConfig = ''
       # set clipboard stuff
       set -g allow-passthrough on
-      set-option -g allow-rename on
+
       # ensure window indexes get reordered on close/delete
       set-option -g renumber-windows on
-    '';
+
+	  # fix prefix indicator for nixos - themes seemingly cannot do this correctly on nixos so this line is needed.
+	  set -g status-left '#{?client_prefix,#[fg=#ff757f],#[fg=#868686]}#[bg=#222436]  ' 
+
+	  # Split windows (opens in same directory)
+	  unbind %
+	  bind | split-window -h -c "#{pane_current_path}"
+
+	  unbind '"'
+	  bind - split-window -v -c "#{pane_current_path}"
+
+	  # Alt+hjkl to switch panes without prefix
+	  bind -n M-h select-pane -L
+	  bind -n M-j select-pane -D
+	  bind -n M-k select-pane -U
+	  bind -n M-l select-pane -R
+
+	  # Shift Left/Right to navigate windows
+	  bind -n S-Left previous-window
+	  bind -n S-Right next-window
+
+	  # these binding need to be here so they load AFTER the resurrect plugin config
+   	  set -g @resurrect-save 'C-s'
+	  set -g @resurrect-restore 'C-r'
+ '';
   };
 }
