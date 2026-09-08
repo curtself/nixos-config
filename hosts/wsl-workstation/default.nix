d6c2d7f2ac90113cad843ccab1e50ee6e1d88473
curtself <curtself.cs@gmail.com>
2026-09-08 12:06:38 -0700

    Added wsl workstation config

---
+++ hosts/wsl-workstation/default.nix
@@ -0,0 +1,42 @@
+{ self, config, pkgs, ... }:
+{
+  imports = [
+    ../../modules/common
+    ../../modules/users/curt.nix
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "curt";
+  wsl.wslConf.network.generateResolvConf = false;
+
+  networking.nameservers = [ "10.5.1.254" ];
+  networking.search = [ "sdccd.loc" ];
+
+  # likely only WSL hosts will need wl-clipboard (for neovim)
+  environment.systemPackages = with pkgs; [
+	wl-clipboard
+  ];
+
+  #environment.variables = {
+  #  XDG_RUNTIME_DIR = "/mnt/wslg/runtime-dir";
+  #  WAYLAND_DISPLAY = "wayland-0";
+  #};
+
+  #systemd.tmpfiles.rules = [
+  #  "L+ /run/user/1000/wayland-0 - - - - /mnt/wslg/runtime-dir/wayland-0"
+  #];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+
+    users.curt = import ../../home/curt.nix;
+  };
+
+  system.configurationRevision = 
+	if self ? rev then
+	  self.rev
+	else "dirty" ;
+
+  system.stateVersion = "26.05";
+}
