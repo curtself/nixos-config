6a66940b9301b67480cdb7ee9fdd3096b792065e
curtself <curtself.cs@gmail.com>
2026-09-08 12:46:28 -0700

    Fixing resolv.conf issues

--- hosts/wsl-workstation/default.nix
+++ hosts/wsl-workstation/default.nix
@@ -14,6 +14,7 @@
   wsl.defaultUser = "curt";
   wsl.wslConf.network.generateResolvConf = false;
 
+  networking.resolvconf.enable = false;
   environment.etc."resolv.conf".text = ''
     nameserver 10.5.1.254
     search sdccd.loc
