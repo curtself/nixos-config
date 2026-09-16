982d5c5c6926fb87cd47aac086cb58395cffde95
curtself <curtself.cs@gmail.com>
2026-09-15 19:49:17 -0700

    Adding new host for samsung tablet

--- flake.nix
+++ flake.nix
@@ -1,72 +1,101 @@
 {
-description = "NixOS Flake";
-inputs = {
-  nixpkgs.url = "nixpkgs/nixos-26.05";
-  home-manager = {
-    url = "github:nix-community/home-manager/release-26.05";
-    inputs.nixpkgs.follows = "nixpkgs";
+  description = "NixOS Flake";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-26.05";
+    };
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  nixos-wsl.url = "github:nix-community/NixOS-WSL";
-  nixvim = {
-	url = "github:nix-community/nixvim/nixos-26.05";
-  };
-  zen-browser = {
-	url = "github:youwen5/zen-browser-flake";
-	inputs.nixpkgs.follows = "nixpkgs";
-  };
-};
 
-outputs = { self, nixpkgs, home-manager, nixos-wsl, nixvim, zen-browser, ... }: {
-  nixosConfigurations.wsl-laptop = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	specialArgs = {
-	  inherit self;
-	};
-	modules = [
-	  ./hosts/wsl-laptop/default.nix
-	  nixos-wsl.nixosModules.default
-	  home-manager.nixosModules.home-manager {
-		home-manager.useGlobalPkgs = true;
-		home-manager.useUserPackages = true;
-		home-manager.sharedModules = [
-		  nixvim.homeManagerModules.nixvim
-		];
-	  }
-    ];
-  };
-  nixosConfigurations.wsl-workstation = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	specialArgs = {
-	  inherit self;
-	};
-	modules = [
-	  ./hosts/wsl-workstation/default.nix
-	  nixos-wsl.nixosModules.default
-	  home-manager.nixosModules.home-manager {
-		home-manager.useGlobalPkgs = true;
-		home-manager.useUserPackages = true;
-		home-manager.sharedModules = [
-		  nixvim.homeManagerModules.nixvim
-		];
-	  }
-	];
-  };
-  nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	specialArgs = {
-	  inherit self zen-browser;
-	};
-	modules = [
-	  ./hosts/nixos-laptop/default.nix
-	  home-manager.nixosModules.home-manager {
-		home-manager.useGlobalPkgs = true;
-		home-manager.useUserPackages = true;
-		home-manager.sharedModules = [
-		  nixvim.homeManagerModules.nixvim
-		];
-	  }
-    ];
-  };
-};
-
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-wsl,
+      nixvim,
+      zen-browser,
+      ...
+    }:
+    {
+      nixosConfigurations.wsl-laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit self;
+        };
+        modules = [
+          ./hosts/wsl-laptop/default.nix
+          nixos-wsl.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [
+              nixvim.homeManagerModules.nixvim
+            ];
+          }
+        ];
+      };
+      nixosConfigurations.wsl-workstation = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit self;
+        };
+        modules = [
+          ./hosts/wsl-workstation/default.nix
+          nixos-wsl.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [
+              nixvim.homeManagerModules.nixvim
+            ];
+          }
+        ];
+      };
+      nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit self zen-browser;
+        };
+        modules = [
+          ./hosts/nixos-laptop/default.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [
+              nixvim.homeManagerModules.nixvim
+            ];
+          }
+        ];
+      };
+      nixosConfigurations.samsung-tablet = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit self zen-browser;
+        };
+        modules = [
+          ./hosts/samsung-tablet/default.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [
+              nixvim.homeManagerModules.nixvim
+            ];
+          }
+        ];
+      };
+    };
 }
---
+++ hosts/samsung-tablet/default.nix
@@ -0,0 +1,36 @@
+# hosts/nixos-laptop/default.nix
+{
+  self,
+  config,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../modules/common
+    ../../modules/users/curt.nix
+    # include the desktop module 
+    ../../modules/desktop
+  ];
+
+  # Laptop-specific NixOS configuration goes here.
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.hostName = "samsung-tablet";
+  networking.networkmanager.enable = true;
+  networking.wireless.enable = true;
+  time.timeZone = "America/Los_Angeles";
+
+  home-manager = {
+    users.curt = import ../../home/curt.nix;
+  };
+
+  system.configurationRevision = if self ? rev then self.rev else "dirty";
+
+  system.stateVersion = "26.05";
+}
